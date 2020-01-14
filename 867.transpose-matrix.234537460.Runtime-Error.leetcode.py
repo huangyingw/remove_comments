eb5fb1365344475b83830aaf18901abc51f864c7
Ying Huang <huangyingw@gmail.com>
2020-01-13 21:53:12 -0800

    n

--- 867.transpose-matrix.234537460.Runtime-Error.leetcode.py
+++ 867.transpose-matrix.234537460.Runtime-Error.leetcode.py
@@ -1,13 +1,5 @@
-# https://leetcode.com/problems/transpose-matrix/description/
 class Solution:
-    # 1
-    # 常规法
-    # O(mn)
     def transpose(self, A):
-        """
-        :type A: List[List[int]]
-        :rtype: List[List[int]]
-        """
         trans = []
         for c in range(len(A[0])):
             temp = []
@@ -15,9 +7,6 @@ class Solution:
                 temp.append(A[r][c])
             trans.append(temp)
         return trans
-    # 2
-    # 提前构造矩阵，利用变化式 a[i][j] = b[j][i]
-
     def transpose(self, A):
         M, N = len(A), len(A[0])
         trans = [[None for m in range(M)] for n in range(N)]
